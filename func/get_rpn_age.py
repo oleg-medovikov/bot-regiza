1e1a39d9ded1c9dafcf492ce15a1c43a7ca55887
oleg-medovikov <oleg.medovikov@mail.ru>
2023-11-01 14:43:09 +0300

    bug with age

--- func/get_rpn_age.py
+++ func/get_rpn_age.py
@@ -8,7 +8,8 @@ def get_rpn_age(birthdate: str, o_303: datetime) -> int:
     ).days
     return {
         delta < 32:        100 + delta,  # меньше месяца
-        32 < delta < 365:  delta // 30 * 100,  # до года
+        32 < delta < 335:  delta // 30 * 100,  # до года
+        334 < delta < 365: 1100,
         364 < delta < 365*3: (100*(delta // 365) + (delta % 365) // 30)*100,
         365*3-1 < delta < 365*100: (delta // 365)*10000,
         365*100-1 < delta:  1010000,
